9b77e6920a7c1a94b58faaf3bc156172a3702adf
Alfredo <manuel.martinez96@hotmail.com>
2020-11-25 12:18:06 -0600

    auth with Google and FB

--- PokeApp/app/src/main/java/com/example/pokeapp/HomeActivity.kt
+++ PokeApp/app/src/main/java/com/example/pokeapp/HomeActivity.kt
@@ -1,13 +1,61 @@
 package com.example.pokeapp
 
-import android.content.Context
+import android.app.LauncherActivity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokeapp.databinding.ActivityHomeBinding
+import com.example.pokeapp.databinding.ActivityMainBinding
+import com.facebook.login.LoginManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+enum class ProviderType {
+    GOOGLE,
+    FACEBOOK
+}
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
+        val bundle= intent.extras
+        val username = bundle?.getString("username")
+        val provider = bundle?.getString("provider")
+        setup(username ?: "", provider ?: "")
+
+        binding.btnLogout.setOnClickListener {  }
+    }
+
+    private fun setup(username: String?, provider: String){
+        title = "Inicio"
+        binding.txtUsername.text = "Hola, $username"
+        binding.btnLogout.setOnClickListener {
+
+            if (provider == ProviderType.FACEBOOK.name){
+                LoginManager.getInstance().logOut()
+            } else if (provider == ProviderType.GOOGLE.name) {
+                signOutFromApp()
+            }
+
+
+        }
+    }
+
+    private fun signOutFromApp() {
+        GoogleSignIn.getClient(
+            this,
+            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        ).signOut()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
--- PokeApp/app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ PokeApp/app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -1,62 +1,121 @@
 package com.example.pokeapp
+
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.example.pokeapp.databinding.ActivityMainBinding
-
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
-    var googleSignInClient : GoogleSignInClient? = null
-    val RC_SIGN_IN = 1000
+    private lateinit var binding: ActivityMainBinding
+    private val GOOGLE_SIGN_IN = 100
+    private val callbackManager = CallbackManager.Factory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
-        val binding:ActivityMainBinding = DataBindingUtil
-            .setContentView(this, R.layout.activity_main)
-
-
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.defaul_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         binding.googlebutton?.setOnClickListener {
-            var signInIntent = googleSignInClient?.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
 
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            //googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
         }
 
+       binding.fbbutton?.setOnClickListener {
+
+           LoginManager.getInstance().logInWithReadPermissions(this, listOf("username"))
+
+           LoginManager.getInstance().registerCallback(callbackManager,
+                object: FacebookCallback<LoginResult> {
+
+                    override fun onSuccess(result: LoginResult?) {
+                        result?.let {
+                            val token = it.accessToken
+
+                            val credential = FacebookAuthProvider.getCredential(token.token)
+
+                            FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{
+                                if (it.isSuccessful){
+                                    showHome(it.result?.user?.email ?: "", ProviderType.FACEBOOK)
+                                }
+                            }
+                        }
+                    }
+
+                    override fun onCancel() {}
+
+                    override fun onError(error: FacebookException?) {
+                        showAlert()
+                    }
+                })
+       }
 
     }
 
-    fun firebaseAuthWithGoogle(acct: GoogleSignInAccount?){
-        var credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
-        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener{ task ->
-            if (task.isSuccessful){
-                print("Google Login Succes")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            showHome(account.displayName?:"",ProviderType.GOOGLE)
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException) {
+                showAlert()
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun showHome(username: String?, provider: ProviderType) {
 
-        if (requestCode == RC_SIGN_IN){
-            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            var account = task.getResult(ApiException::class.java)
-            firebaseAuthWithGoogle(account)
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("username", username)
+            putExtra("provider", provider.name)
         }
+        startActivity(homeIntent)
+    }
+
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error de autenticación al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
     }
 }
