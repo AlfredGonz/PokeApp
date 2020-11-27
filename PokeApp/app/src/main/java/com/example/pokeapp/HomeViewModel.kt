6c8e0b6a08973902f3e8a1e6f1d5ff36ab4b2dbe
Alfredo <manuel.martinez96@hotmail.com>
2020-11-26 19:46:07 -0600

    shows list of regions

--- PokeApp/app/src/main/java/com/example/pokeapp/HomeViewModel.kt
+++ PokeApp/app/src/main/java/com/example/pokeapp/HomeViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.pokeapp.ui.Regiones
 import com.example.pokeapp.ui.service
 import kotlinx.coroutines.*
+import org.json.JSONObject
 
 class HomeViewModel : ViewModel(){
 
@@ -25,9 +26,26 @@ class HomeViewModel : ViewModel(){
     private suspend fun fetchRegiones() : MutableList<Regiones> {
         return withContext(Dispatchers.IO){
             val rgListString = service.getRegiones()
-
-            Log.d("REGIONES", rgListString)
-            mutableListOf<Regiones>()
+            val regionesList = parseRegionesResult(rgListString)
+            
+            regionesList
         }
+    }
+
+    private fun parseRegionesResult(rgListString: String): MutableList<Regiones> {
+        val rgJsonObject = JSONObject(rgListString)
+        val resultJsonArray = rgJsonObject.getJSONArray("results")
+
+        val regionesList = mutableListOf<Regiones>()
+
+        for (i in 0 until resultJsonArray.length()) {
+            val resultJsonObject = resultJsonArray[i] as JSONObject
+            val nombre = resultJsonObject.getString("name")
+
+            val region = Regiones(nombre)
+            regionesList.add(region)
+        }
+
+        return regionesList
     }
 }
